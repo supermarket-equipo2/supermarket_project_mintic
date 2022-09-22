f5e7ae6e891fa6a438f49f1363c542ceb7fa09f0
Juan Sebastián Castaño Franco <jcastanof@unal.edu.co>
2022-09-22 16:56:05 -0500

    Third commit

---
+++ superJS.js
@@ -0,0 +1,14 @@
+// Enlazar Log in con la ventana de ingresar.
+const open = document.getElementById('open');
+const log_in = document.getElementById('log_in');
+
+open.addEventListener('click', () => {
+    open.classList.add('show');
+    alert('prueba');
+});
+
+log.addEventListener('click', () => {
+    log_in.classList.remove('show');
+});
+
+
